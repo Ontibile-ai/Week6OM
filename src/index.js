7ff513362754cff5f18ba1b5050fc08ef1cbc3cb
Ontibile-ai <ontmmusi@gmail.com>
2026-07-25 22:13:08 +0200

    footer

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "cbab4a1ta01fc9fe9f10bf357f41o145";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
